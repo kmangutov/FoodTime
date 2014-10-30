e3dca35c4a9e14f42e48ae93d6b1a5db5ba2b799
kmangutov <kmangutov@gmail.com>
2014-10-30 16:19:03 -0500

    delete .idea folder

--- app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeSlot.java
+++ app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeSlot.java
@@ -4,4 +4,15 @@ package com.kmangutov.foodtime.TimeBar;
  * Created by kmangutov on 10/30/14.
  */
 public class TimeSlot {
+    protected int mHour = -1;
+    protected int mMinutes = -1;
+
+    public TimeSlot() {
+
+    }
+
+    public TimeSlot(int hours, int minutes) {
+        mHour = hours;
+        mMinutes = minutes;
+    }
 }
