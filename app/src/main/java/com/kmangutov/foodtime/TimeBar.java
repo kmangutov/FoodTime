cda1cfdfaf3a17610d4cc4c902e62af19cecf92c
kmangutov <kmangutov@gmail.com>
2014-10-29 19:16:17 -0500

    listen to ontouch and change selection %

--- app/src/main/java/com/kmangutov/foodtime/TimeBar.java
+++ app/src/main/java/com/kmangutov/foodtime/TimeBar.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -19,16 +21,37 @@ public class TimeBar extends View {
         super(context, attrs);
     }
 
+    double selection = 0.0;
+
     @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
 
         Paint paint = new Paint();
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL);
         paint.setColor(0xFF33B5E5);
         paint.setStrokeWidth(4);
-        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
-        //paint.setColor(0x777777);
+
+        int barHeight = (int)(selection * getHeight());
+        canvas.drawRect(0, barHeight, getWidth(), getHeight(), paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(0x000);
         canvas.drawLine(0, 0, getWidth(), getHeight(), paint);
+        this.invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("MOTIONEVENT", event.getX() + "," + event.getY());
+                selection = event.getY()/getHeight();
+                System.out.println("selection: " + selection);
+        }
+
+        return true;
     }
 }
