7a962d5479178278f9d17730aa27ff0e49ca9a24
mntox1295 <mntox1295@gmail.com>
2014-10-30 17:03:20 -0500

    we're doneeeeeeeeeeee

--- app/src/main/java/com/kmangutov/foodtime/MainActivity.java
+++ app/src/main/java/com/kmangutov/foodtime/MainActivity.java
@@ -1,9 +1,13 @@
 package com.kmangutov.foodtime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -11,7 +15,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
     }
 
 
@@ -32,5 +36,40 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login_button(View v){
+
+        EditText username_input = (EditText)findViewById(R.id.username_input);
+        EditText password_input = (EditText)findViewById(R.id.password_input);
+
+        if(!username_input.getText().toString().trim().equals("") && !password_input.getText().toString().trim().equals(""))
+        {
+            //String output="";
+            String password = password_input.getText().toString();
+
+
+            String username = username_input.getText().toString();
+
+            /*
+            String sRequest = "http://web.engr.illinois.edu/~null_ptrs/bpoints/user_table/get_user.php?username="+ username +"&password="+ password;
+            Log.w("sRequest", sRequest);
+            output = Process_request.runProcess(sRequest);
+            */
+
+            if(!username_input.equals("")&&!password_input.equals(""))
+            {
+                //set_user(username, password);
+                startActivity(new Intent(MainActivity.this, SchedulingActivity.class));
+            }
+            else
+            {
+                Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+        else
+            Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show();
     }
 }
