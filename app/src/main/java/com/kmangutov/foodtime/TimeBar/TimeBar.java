881703844e84886a7a272fffe4650c7eba5d8f19
kmangutov <kmangutov@gmail.com>
2014-11-01 17:13:53 -0500

    timebar looks prettier and clicking+Dragging creates new timeslots

--- app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeBar.java
+++ app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeBar.java
@@ -6,8 +6,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
+
+import org.joda.time.LocalTime;
 
 import java.util.ArrayList;
 
@@ -16,7 +19,8 @@ import java.util.ArrayList;
  */
 public class TimeBar extends View {
 
-    ArrayList<TimeSlot> mTimeSlots;
+    ArrayList<TimeSlot> mTimeSlots = new ArrayList<TimeSlot>();
+    TimeSlot selected = null;
 
     public TimeBar(Context context) {
         super(context);
@@ -27,58 +31,134 @@ public class TimeBar extends View {
     }
 
     double selection = 0.0;
-    double release = 0.0;
 
-    protected void fractionToTime(float fraction) {
+    protected int mBarWidth = 180;
+    protected int mBarLeftPadding;
 
+    protected LocalTime fractionToTime(float fraction) {
+        int hours = (int) Math.floor(fraction * 24);
+        int minutes = (int) Math.floor( (fraction * 24 ) % 1 );
+
+        return new LocalTime(hours, minutes);
+    }
+
+    protected int getBarWidth() {
+        return Math.min(getWidth(), mBarWidth);
+    }
+
+    protected int getBarX() {
+        return getWidth() - getBarWidth();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
 
+
+        drawBar(canvas);
+        drawTicks(canvas);
+        drawTimeSlots(canvas);
+    }
+
+    protected void drawTimeSlots(Canvas canvas) {
+
+        Paint paint = new Paint();
+        paint.setAlpha(255);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(4);
+
+        for(TimeSlot slot : mTimeSlots) {
+            canvas.drawRect(
+                    getBarX(),
+                    getHeight()*slot.start,
+                    getBarX() + getBarWidth(),
+                    getHeight()*slot.end,
+                    paint);
+        }
+    }
+
+    protected void drawTicks(Canvas canvas) {
         Paint paint = new Paint();
         paint.setAlpha(255);
 
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.BLACK);
+        paint.setTextSize(20);
+
+        for(int i = 0; i < 24; i++) {
+
+            String str = String.format("%02d", i);
+
+            canvas.drawText(str, getBarX() - 30, 20 + i*getHeight()/24, paint);
+        }
+    }
+
+    protected void drawBar(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setAlpha(255);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
         paint.setStrokeWidth(4);
-        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+        canvas.drawRect(getBarX(), 0, getBarX() + getBarWidth(), getHeight(), paint);
+    }
 
-        this.invalidate();
+    protected TimeSlot touchYFracToTimeSlot(float fraction) {
+        // TODO: loop through mTimeSlots and check if we selected a timeslot and return it
+        // return null if we didn't
 
-
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(0xFF33B5E5);
-        paint.setStrokeWidth(4);
-
-        int barTop = (int)(selection * getHeight());
-	int barBottom = (int)(release * getHeight());
-        canvas.drawRect(15, barTop, getWidth(), barBottom, paint);
-
-	paint.setColor(0xFF23A5D5);
-	canvas.drawText(selection, 15, barTop, paint);
-	canvas.drawText(release, 15, barBottom, paint);
-	
-        this.invalidate();
+        return null;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        if(eventX < getBarX() || eventX > getBarX() + getBarWidth())
+            return false;
+
+        float yFrac = eventY / getHeight();
+
         switch(event.getAction())
         {
             case MotionEvent.ACTION_DOWN:
-                Log.d("MOTIONEVENT", event.getX() + "," + event.getY());
-                selection = event.getY()/getHeight();
-                System.out.println("selection: " + selection);
-		break;
-	    case MotionEvent.ACTION_UP:
-		Log.d("MOTIONEVENT", event.getX() + "," + event.getY());
-		release = event.getY()/getHeight();
-		System.out.println("release: " + release);
-		break;
+                /*  check if we clicked on an existing TimeSlot. if so select this timeslot
+                    and begin manipulation of it.
+                    somehow keep track whether top, middle or bottom is selected (change start value
+                    vs change overall time vs change end value )
+
+                    if it doesn't exist, create a new TimeSlot and select it */
+
+                selected = touchYFracToTimeSlot(yFrac);
+
+                if(selected == null) {
+                    selected = new TimeSlot(yFrac, yFrac);
+                    mTimeSlots.add(selected);
+                }
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                selected = null;
+
+                /*  unselect selected timeslot */
+                break;
+            case MotionEvent.ACTION_MOVE:
+                /*  either change start time (top selected), end time (end selected)
+                    or both if middle is selected
+                 */
+                selected.end = yFrac;
+
+                break;
+
+
         }
+        this.invalidate();
 
         return true;
     }
--- app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeSlot.java
+++ app/src/main/java/com/kmangutov/foodtime/TimeBar/TimeSlot.java
@@ -7,9 +7,19 @@ public class TimeSlot {
     protected LocalTime mBeginTime;
     protected LocalTime mEndTime;
 
+    //these are fractions 0 to 1 mapping 0 hrs to 24 hrs
+    public float start;
+    public float end;
+
     public TimeSlot() {
 
     }
+
+    public TimeSlot(float start, float end) {
+        this.start = start;
+        this.end = end;
+    }
+
     public LocalTime getBeginTime() {
         return mBeginTime;
     }
