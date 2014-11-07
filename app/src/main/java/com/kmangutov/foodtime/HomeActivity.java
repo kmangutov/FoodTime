337935b823ebcd40b9b9eea5d7a0188d6b55ab1c
izmaxx <isunhachawi@gmail.com>
2014-11-07 00:22:35 -0500

    Layout for home screen

---
+++ app/src/main/java/com/kmangutov/foodtime/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.kmangutov.foodtime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_activity_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_person:
+                startActivity(new Intent(HomeActivity.this, FriendActivity.class));
+                return true;
+            // Can add other actions here
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
