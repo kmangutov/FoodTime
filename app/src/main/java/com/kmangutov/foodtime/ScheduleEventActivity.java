297131c0a1c5afbc35abcdacc4d910ea38593efe
hkiang01 <hkiang2@illinois.edu>
2014-12-03 23:58:30 -0600

    kir feelz

--- app/src/main/java/com/kmangutov/foodtime/ScheduleEventActivity.java
+++ app/src/main/java/com/kmangutov/foodtime/ScheduleEventActivity.java
@@ -14,6 +14,12 @@ import java.util.Vector;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.kmangutov.foodtime.TimeBar.TimeBar;
+import com.kmangutov.foodtime.TimeBar.TimeSlot;
+
+import org.joda.time.LocalTime;
 
 public class ScheduleEventActivity extends Activity implements TabListener{
 
@@ -25,6 +31,7 @@ public class ScheduleEventActivity extends Activity implements TabListener{
     Button mButton;
     EditText mEdit0;
     EditText mEdit1;
+    TimeBar mTimeBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,8 +109,51 @@ public class ScheduleEventActivity extends Activity implements TabListener{
         mEdit0   = (EditText)findViewById(R.id.inputMeetingTitle);
         mEdit1 = (EditText)findViewById(R.id.inputLocation);
 
-        System.out.println("inputMeetingTitle: " + mEdit0.getText().toString());
-        System.out.println("inputLocation: " + mEdit1.getText().toString());
+        mTimeBar = (TimeBar)findViewById(R.id.timeBar);
+        TimeSlot slot;
+        int start_hour = 0;
+        int start_minute = 0;
+        int end_hour = 0;
+        int end_minute = 0;
+        if(mTimeBar.getTimeSlot()!=null)
+        {
+            slot = mTimeBar.getTimeSlot();
+            LocalTime start_time = slot.getBeginTime();
+            LocalTime end_time = slot.getEndTime();
+            if(start_time!=null)
+            {
+                start_hour = start_time.getHourOfDay();
+                start_minute = start_time.getMinuteOfHour();
+            }
+            else
+            {
+                System.out.println("start_time is null");
+            }
+
+            if(end_time!=null)
+            {
+                end_hour = end_time.getHourOfDay();
+                end_minute = end_time.getMinuteOfHour();
+            }
+            else
+            {
+                System.out.println("end_time is null");
+            }
+
+            System.out.println("inputMeetingTitle: " + mEdit0.getText().toString());
+            System.out.println("inputLocation: " + mEdit1.getText().toString());
+            System.out.println("Start Time: " + Integer.toString(start_hour) + ":" + Integer.toString(start_minute));
+            System.out.println("End Time: " + Integer.toString(end_hour) + ":" + Integer.toString(end_minute));
+        }
+        else
+        {
+            Toast.makeText(this, "Invalid timeslot", Toast.LENGTH_LONG);
+            return;
+        }
+
+
+
+
 
         finish();
     }
